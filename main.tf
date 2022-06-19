900e6bd54e7d880d07dc19ae10b1093213675028
Raul de la Cruz <rcruz2@mapfre.com>
2022-06-19 20:56:33 +0200

    New: Add s3_acl

--- main.tf
+++ main.tf
@@ -3,14 +3,24 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+
 /* create s3 bucket, WEB, public read, index.html default */
 resource "aws_s3_bucket" "web" {
   bucket = var.s3_name
-  acl = "public-read"
 }
 /* resource aws_s3_bucket_website_configuration */
 resource "aws_s3_bucket_website_configuration" "web" {
   bucket = aws_s3_bucket.web.id
-  error_document = var.error_name
-  index_document = var.index_name
+  error_document {
+    key = var.error_name
+  }
+  index_document {
+    suffix = var.index_name
+  }
+}
+
+/* resource aws_s3_bucket_acl */
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl = "public-read"
 }
