36c46c875c65291dbc4b468144c2fc755bb00d66
Raul de la Cruz <rcruz2@mapfre.com>
2022-06-19 20:34:59 +0200

    Fix: Correct a syntax error in variables.tf The type doesn't need to be quoted

--- variables.tf
+++ variables.tf
@@ -1,15 +1,9 @@
 /* string variable s3 name */
 variable "s3_name" {
-  type = "string"
-}
-
-/* string variable s3 region */
-variable "s3_region" {
-  type = "string"
-  default = "eu-west-1"
+  type = string
 }
 
 /* string variable index name */
 variable "index_name" {
-    type = "string"
+    type = string
 }
